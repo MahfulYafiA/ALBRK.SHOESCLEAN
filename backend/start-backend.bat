31aa5c2ff12f32af553be52efa66f1a6ea04d661
MahfulYafiA <mahfulpribadi@gmail.com>
2026-07-13 15:55:14 +0700

    Restructure Laravel into backend directory

--- backend/start-backend.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-set "APP_ROOT=%~dp0.."
-set "TEMP=%APP_ROOT%\storage\framework\tmp"
-set "TMP=%APP_ROOT%\storage\framework\tmp"
-php artisan serve
--- backend/start-backend.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Set-Location $PSScriptRoot
-$appRoot = Resolve-Path "$PSScriptRoot\.."
-$tempPath = Join-Path $appRoot 'storage\framework\tmp'
-$env:TEMP = $tempPath
-$env:TMP = $tempPath
-php artisan serve
